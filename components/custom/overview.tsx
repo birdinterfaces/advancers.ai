5193253ed5435bbc47626866656bf6e596bfd567
birdinterface <alexgisbrecht20@gmail.com>
2024-10-25 13:05:34 +0200

    Removed Icons

--- components/custom/overview.tsx
+++ components/custom/overview.tsx
@@ -14,11 +14,6 @@ export const Overview = () => {
       transition={{ delay: 0.5 }}
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
-        </p>
         <p>
           Nova
         </p>
