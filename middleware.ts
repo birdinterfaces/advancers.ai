da9bfc02f943c978e8d67c94fc4b468c46841abf
birdinterface <alexgisbrecht20@gmail.com>
2024-10-30 17:36:12 +0100

    middleware adjustment for stripe webhooks redirects

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
 import NextAuth from "next-auth";
-
 import { authConfig } from "@/app/(auth)/auth.config";
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ["/", "/:id", "/api/:path*", "/login", "/register"],
+  matcher: [
+    // Apply middleware to these routes
+    '/',
+    '/:id',
+    '/login',
+    '/register',
+    '/api/:path*',
+    // Exclude the Stripe webhook endpoint
+    '!/api/stripe/webhooks',
+  ],
 };
