b586602c400df88cb6420f886d4ef94fc33bfb88
birdinterface <alexgisbrecht20@gmail.com>
2024-10-30 17:46:37 +0100

    v2

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,13 @@
 import NextAuth from "next-auth";
+
 import { authConfig } from "@/app/(auth)/auth.config";
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    // Apply middleware to these routes
-    '/',
-    '/:id',
-    '/login',
-    '/register',
-    '/api/:path*',
-    // Exclude the Stripe webhook endpoint
-    '!/api/stripe/webhooks',
+    // Match all routes except the webhook endpoint
+    '/((?!api/stripe/webhooks).*)',
   ],
 };
+
