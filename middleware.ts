218ede4a5b9843803c59074947eb9aa99611a50c
birdinterface <alexgisbrecht20@gmail.com>
2024-10-31 08:57:10 +0100

    middleware adjustment

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,5 @@ import { authConfig } from "@/app/(auth)/auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: [
-    // Match all routes except the webhook endpoint
-    '/((?!api/stripe/webhooks).*)',
-  ],
+  matcher: ["/", "/:id", "/login", "/register"],
 };
-
