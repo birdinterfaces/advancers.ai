309c69ef536d15e80e2ff34861f6c68c3693fe47
birdinterface <alexgisbrecht20@gmail.com>
2024-11-05 17:54:29 +0100

    v4

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'avatar.vercel.sh',
+      'lh3.googleusercontent.com',
+    ],
+  },
+  // ... other config options
+}
+
+module.exports = nextConfig 
