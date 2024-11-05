84c49be038356b0ca206d1518d546e54977bb0eb
birdinterface <alexgisbrecht20@gmail.com>
2024-11-05 15:41:41 +0100

    v3

--- components/custom/sidebar-user-nav.tsx
+++ components/custom/sidebar-user-nav.tsx
@@ -230,7 +230,7 @@ export function SidebarUserNav({ user }: { user: ExtendedUser }) {
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton className="data-[state=open]:bg-sidebar-accent bg-background data-[state=open]:text-sidebar-accent-foreground h-10">
               <Image
-                src={`https://avatar.vercel.sh/${user.email}`}
+                src={user.image || `https://avatar.vercel.sh/${user.email}`}
                 alt={user.email ?? 'User Avatar'}
                 width={24}
                 height={24}
